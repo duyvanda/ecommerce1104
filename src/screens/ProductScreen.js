5bc9b01b7a6cbb83c0fadb1134502d7e49eb1fc3
Duy Van <vanquangduy10@gmail.com>
2022-04-11 22:55:09 +0700

    2.10

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import products from "../products";
 
-function ProductScreen() {
-	return <div>Product</div>;
+function ProductScreen({ match }) {
+	const product = products.find((p) => p._id == match.params.id);
+	return (
+		<div>
+			<Link to="/" className="btn btn-primary my-3">
+				Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image src={product.image} alt={product.name} fluid />
+				</Col>
+				<Col md={3}>
+					<ListGroup variant="flush">
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							{product.rating} from {product.numReviews} reviews
+						</ListGroup.Item>
+
+						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+
+				<Col md={3}>
+					<ListGroup variant="flush">
+						<ListGroup.Item>
+							<h3>{product.price}</h3>
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							Status: {product.countInStock > 0 ? "Instock" : "Out Of Stock"}
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<div className="d-grid gap-2">
+								<Button
+									variant="primary"
+									size="md"
+									disabled={product.countInStock == 0}
+								>
+									ADD TO CART
+								</Button>
+							</div>
+						</ListGroup.Item>
+					</ListGroup>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
 export default ProductScreen;
