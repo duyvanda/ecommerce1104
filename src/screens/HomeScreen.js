3c21badf574853c462398ba896e186147aaa1939
Duy Van <vanquangduy10@gmail.com>
2022-04-12 21:39:51 +0700

    3.13

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
+// import products from "../products";
 import Product from "../components/Product";
 function HomeScreen() {
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		async function fetchProducts() {
+			const { data } = await axios.get("http://127.0.0.1:8000/api/products");
+			setProducts(data);
+		}
+
+		fetchProducts();
+	}, []);
 	return (
 		<div>
 			<h1>Latest Product</h1>
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
-import products from "../products";
+// import products from "../products";
+import axios from "axios";
 
 function ProductScreen({ match }) {
-	const product = products.find((p) => p._id == match.params.id);
+	// const product = products.find((p) => p._id == match.params.id);
+
+	const [product, setProduct] = useState([]);
+	useEffect(() => {
+		async function fetchProduct() {
+			const { data } = await axios.get(
+				`http://127.0.0.1:8000/api/product/${match.params.id}`
+			);
+			setProduct(data);
+		}
+
+		fetchProduct();
+	}, []);
+
 	return (
 		<div>
 			<Link to="/" className="btn btn-primary my-3">
