20813fd88777258800b32a9ae8acc7883e1d93f1
Duy Van <vanquangduy10@gmail.com>
2022-04-14 23:03:35 +0700

    4.22

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export const listProducts = () => async (dispatch) => {
 	try {
-		dispatch({ type: PRODUCT_LIST_REQUEST });
+		dispatch({ type: "PRODUCT_LIST_REQUEST" });
 		const { data } = await axios.get("http://127.0.0.1:8000/api/products");
 		dispatch({
-			type: PRODUCT_LIST_REQUEST,
+			type: "PRODUCT_LIST_SUCCESS",
 			payload: data,
 		});
 	} catch (error) {
 		dispatch({
-			type: PRODUCT_LIST_FAIL,
-			payload: error.response.data.message,
+			type: "PRODUCT_LIST_FAIL",
+			payload: error.message,
 		});
 	}
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,32 @@
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-// import products from "../products";
 import Product from "../components/Product";
-function HomeScreen() {
-	const [products, setProducts] = useState([]);
-	useEffect(() => {
-		async function fetchProducts() {
-			const { data } = await axios.get("http://127.0.0.1:8000/api/products");
-			setProducts(data);
-		}
+import { listProducts } from "../actions/productActions";
 
-		fetchProducts();
-	}, []);
+function HomeScreen() {
+	// const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { error, loading, products } = productList;
+	useEffect(() => {
+		dispatch(listProducts());
+	}, [dispatch]);
+
+	const ROW_PRODUCT = (
+		<Row>
+			{products.map((product) => (
+				<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+					<Product product={product} />
+				</Col>
+			))}
+		</Row>
+	);
+
 	return (
 		<div>
 			<h1>Latest Product</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> : ROW_PRODUCT}
 		</div>
 	);
 }
