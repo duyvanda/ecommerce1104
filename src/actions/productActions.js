ebb4c2cec5cccaa3a58cd03a73e5e502f7d7edc7
Duy Van <vanquangduy10@gmail.com>
2022-04-14 22:26:57 +0700

    4.21

---
+++ src/actions/productActions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const listProducts = () => async (dispatch) => {
+	try {
+		dispatch({ type: PRODUCT_LIST_REQUEST });
+		const { data } = await axios.get("http://127.0.0.1:8000/api/products");
+		dispatch({
+			type: PRODUCT_LIST_REQUEST,
+			payload: data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PRODUCT_LIST_FAIL,
+			payload: error.response.data.message,
+		});
+	}
+};
